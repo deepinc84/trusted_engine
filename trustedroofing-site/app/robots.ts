44a0de469b01545b1cf301df434f1db34fcbc114
deepinc84 <deepinc@live.com>
2026-06-10 14:36:25 -0600

    Update robots.ts

--- trustedroofing-site/app/robots.ts
+++ trustedroofing-site/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/solar-suitability"]
     },
-    sitemap: "https://www.trustedexteriors.ca/sitemap.xml"
+    sitemap: "https://www.trustedroofingcalgary.com/sitemap.xml"
   };
 }
